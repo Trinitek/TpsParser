1e3cb90526bb99fcc7a19c21104bc4f848797f78
Blake Burgess <trinitek@live.com>
2019-06-26 10:35:39 -0400

    Add MemoRecord

---
+++ TpsParser/Tps/Record/MemoRecord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TpsParser.Binary;
+using TpsParser.Tps.Header;
+
+namespace TpsParser.Tps.Record
+{
+    public sealed class MemoRecord
+    {
+        private MemoHeader Header { get; }
+        private RandomAccess Data { get; }
+
+        public int Owner => Header.OwningRecord;
+
+        public MemoRecord(MemoHeader header, RandomAccess rx)
+        {
+            Header = header ?? throw new ArgumentNullException(nameof(header));
+            Data = rx ?? throw new ArgumentNullException(nameof(rx));
+        }
+
+        public string GetDataAsMemo() =>
+            Encoding.GetEncoding("ISO-8859-1").GetString(Data.GetData());
+
+        public IEnumerable<byte> GetDataAsBlob() =>
+            Data.ReadBytes(Data.LongLE());
+    }
+}

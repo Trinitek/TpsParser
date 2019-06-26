9fc84dc19dc23a04938ba39a3f6fc4575af7e6d2
Blake Burgess <trinitek@live.com>
2019-06-26 10:37:50 -0400

    Add TableNameRecord

---
+++ TpsParser/Tps/Record/TableNameRecord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TpsParser.Tps.Header;
+
+namespace TpsParser.Tps.Record
+{
+    public sealed class TableNameRecord
+    {
+        private TableNameHeader Header { get; }
+        private int TableNumber { get; }
+
+        public TableNameRecord(TpsRecord record)
+        {
+            if (record == null)
+            {
+                throw new ArgumentNullException(nameof(record));
+            }
+
+            Header = (TableNameHeader)record.Header;
+            TableNumber = record.Data.LongBE();
+        }
+
+        public override string ToString()
+        {
+            return $"TableRecord({Header.Name},{TableNumber})";
+        }
+    }
+}

bd2505aa89f173dcb239138232a361b550b75f10
Blake Burgess <trinitek@live.com>
2019-06-26 10:30:16 -0400

    Add IndexRecord

---
+++ TpsParser/Tps/Record/IndexRecord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TpsParser.Tps.Header;
+
+namespace TpsParser.Tps.Record
+{
+    public sealed class IndexRecord
+    {
+        private IndexHeader Header { get; }
+        public int RecordNumber { get; }
+
+        public IndexRecord(TpsRecord tpsRecord)
+        {
+            if (tpsRecord == null)
+            {
+                throw new ArgumentNullException(nameof(tpsRecord));
+            }
+
+            Header = (IndexHeader)tpsRecord.Header;
+
+            var data = tpsRecord.Data;
+            data.JumpAbsolute(data.Length - 4);
+
+            RecordNumber = data.LongBE();
+        }
+
+        public override string ToString() =>
+            $"IndexRecord({Header.TableNumber},{Header.IndexNumber},{RecordNumber})";
+    }
+}

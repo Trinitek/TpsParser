f6e8d711df8d221e1197741ea91bf545dd9d0e78
Blake Burgess <trinitek@live.com>
2019-06-26 09:15:02 -0400

    Add MemoDefinitionRecord

---
+++ TpsParser/Tps/Record/MemoDefinitionRecord.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TpsParser.Binary;
+
+namespace TpsParser.Tps.Record
+{
+    public sealed class MemoDefinitionRecord
+    {
+        private string ExternalFile { get; }
+        public string Name { get; }
+        private int Length { get; }
+        public int Flags { get; }
+
+        public bool IsMemo => (Flags & 0x04) == 0;
+        public bool IsBlob => !IsMemo;
+
+        public MemoDefinitionRecord(RandomAccess rx)
+        {
+            if (rx == null)
+            {
+                throw new ArgumentNullException(nameof(rx));
+            }
+
+            ExternalFile = rx.ZeroTerminatedString();
+
+            if (ExternalFile.Length == 0)
+            {
+                if (rx.Byte() != 0)
+                {
+                    throw new ArgumentException("Bad memo definition: missing 0x01 after zero string.");
+                }
+            }
+
+            Name = rx.ZeroTerminatedString();
+            Length = rx.ShortLE();
+            Flags = rx.ShortLE();
+        }
+
+        public override string ToString()
+        {
+            return $"MemoDefinition({ExternalFile},{Name},{Length},{Flags})";
+        }
+    }
+}

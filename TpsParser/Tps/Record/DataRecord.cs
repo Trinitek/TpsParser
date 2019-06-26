bb2be7aca0b994049f4e5813d8c78e62c6ede021
Blake Burgess <trinitek@live.com>
2019-06-26 10:23:31 -0400

    Add DataRecord

---
+++ TpsParser/Tps/Record/DataRecord.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using TpsParser.Tps.Header;
+
+namespace TpsParser.Tps.Record
+{
+    public sealed class DataRecord
+    {
+        private DataHeader Header { get; }
+        public TableDefinitionRecord TableDefinition { get; }
+        public IEnumerable<object> Values { get; }
+        public TpsRecord Record { get; }
+
+        public int RecordNumber => Header.RecordNumber;
+
+        public DataRecord(TpsRecord tpsRecord, TableDefinitionRecord tableDefinition)
+        {
+            Record = tpsRecord ?? throw new System.ArgumentNullException(nameof(tpsRecord));
+            TableDefinition = tableDefinition ?? throw new System.ArgumentNullException(nameof(tableDefinition));
+            Header = (DataHeader)Record.Header;
+            Values = TableDefinition.Parse(tpsRecord.Data.Remainder());
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.Append($"{RecordNumber} :");
+
+            foreach (var value in Values)
+            {
+                sb.Append($" {value}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}

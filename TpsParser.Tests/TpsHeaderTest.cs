02aa744bc10e2b458a2b4ed9ee973525b34b5cc7
Blake Burgess <trinitek@live.com>
2019-06-29 23:57:41 -0400

    Implement TpsHeaderTest

--- TpsParser.Tests/TpsHeaderTest.cs
+++ TpsParser.Tests/TpsHeaderTest.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using System.IO;
+using TpsParser.Tps;
 
 namespace TpsParser.Tests
 {
@@ -8,7 +10,24 @@ namespace TpsParser.Tests
         [Test]
         public void ShouldParseHeader()
         {
-            // TODO
+            var file = new TpsFile(new FileStream("Resources/header.dat", FileMode.Open));
+
+            var header = file.GetHeader();
+
+            Assert.IsTrue(header.IsTopSpeedFile);
+            Assert.AreEqual(383744, header.FileLength1);
+            Assert.AreEqual(5048, header.LastIssuedRow);
+            Assert.AreEqual(15651, header.Changes);
+            Assert.AreEqual(60, header.PageStart.Count);
+            Assert.AreEqual(60, header.PageEnd.Count);
+        }
+
+        [Test]
+        public void ShouldNotParseHeaderIfNotTopSpeed()
+        {
+            var file = new TpsFile(new FileStream("Resources/bad-header.dat", FileMode.Open));
+
+            Assert.Throws<NotATopSpeedFileException>(() => file.GetHeader());
         }
     }
 }

95878b24af127c4ddc7080bee8e57d5f7ba120a1
Blake Burgess <trinitek@live.com>
2019-07-06 05:17:46 -0400

    Add RecoveryState and RecoveryStateExtensions

---
+++ TpsParser/Tps/KeyRecovery/RecoveryState.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TpsParser.Tps.KeyRecovery
+{
+    public sealed class RecoveryState : IComparable<RecoveryState>, IEquatable<RecoveryState>
+    {
+        public RecoveryState Parent { get; }
+
+        public PartialKey PartialKey { get; }
+
+        private Block EncryptedHeaderBlock { get; }
+        private Block PlaintextHeaderBlock { get; }
+
+        public IEnumerable<Block> B0Blocks { get; }
+        public IEnumerable<Block> SequentialBlocks { get; }
+
+        private RecoveryState(RecoveryState parent, PartialKey partialKey, IEnumerable<Block> b0Blocks, IEnumerable<Block> sequentialBlocks, Block encryptedHeaderBlock, Block plaintextHeaderBlock)
+        {
+            Parent = parent ?? throw new ArgumentNullException(nameof(parent));
+            PartialKey = partialKey ?? throw new ArgumentNullException(nameof(partialKey));
+            B0Blocks = b0Blocks ?? throw new ArgumentNullException(nameof(b0Blocks));
+            SequentialBlocks = sequentialBlocks ?? throw new ArgumentNullException(nameof(sequentialBlocks));
+            EncryptedHeaderBlock = encryptedHeaderBlock ?? throw new ArgumentNullException(nameof(encryptedHeaderBlock));
+            PlaintextHeaderBlock = plaintextHeaderBlock ?? throw new ArgumentNullException(nameof(plaintextHeaderBlock));
+        }
+
+        private RecoveryState(PartialKey partialKey, IEnumerable<Block> b0Blocks, IEnumerable<Block> sequentialBlocks, Block encryptedHeaderBlock, Block plaintextHeaderBlock)
+            : this(
+                  parent: null,
+                  partialKey: partialKey,
+                  b0Blocks: b0Blocks,
+                  sequentialBlocks: sequentialBlocks,
+                  encryptedHeaderBlock: encryptedHeaderBlock,
+                  plaintextHeaderBlock: plaintextHeaderBlock)
+        { }
+
+        private RecoveryState(RecoveryState parent, PartialKey partialKey, Block partiallyDecryptedBlock)
+            : this(
+                  parent: parent,
+                  partialKey: partialKey,
+                  b0Blocks: parent.B0Blocks,
+                  sequentialBlocks: parent.SequentialBlocks,
+                  encryptedHeaderBlock: partiallyDecryptedBlock,
+                  plaintextHeaderBlock: parent.PlaintextHeaderBlock)
+        { }
+
+        internal RecoveryState(RecoveryState parent, IEnumerable<Block> b0Blocks, IEnumerable<Block> sequentialBlocks)
+            : this(
+                  parent: parent,
+                  partialKey: parent.PartialKey,
+                  b0Blocks: b0Blocks,
+                  sequentialBlocks: sequentialBlocks,
+                  encryptedHeaderBlock: parent.EncryptedHeaderBlock,
+                  plaintextHeaderBlock: parent.PlaintextHeaderBlock)
+        { }
+
+        internal static RecoveryState CreateFromSequentialBlocks(RecoveryState parent, IEnumerable<Block> sequentialBlocks) =>
+            new RecoveryState(parent, parent.B0Blocks, sequentialBlocks);
+
+        internal static RecoveryState CreateFromB0Blocks(RecoveryState parent, IEnumerable<Block> b0blocks) =>
+            new RecoveryState(parent, b0blocks, parent.SequentialBlocks);
+
+        /// <summary>
+        /// Scans the given key index to find a set of potential values that can decrypt the header block. Takes all 64 bits into consideration.
+        /// </summary>
+        /// <param name="keyIndex"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<RecoveryState>> IndexScan(int keyIndex, CancellationToken cancellationToken)
+        {
+            var results = await PartialKey.KeyIndexScan(keyIndex, EncryptedHeaderBlock, PlaintextHeaderBlock, cancellationToken);
+
+            return results.Select(r => new RecoveryState(this, r.Key, r.Value));
+        }
+
+        public async Task<IEnumerable<RecoveryState>> IndexSelfScan(int keyIndex, CancellationToken cancellationToken)
+        {
+            var results = await PartialKey.KeyIndexSelfScan(keyIndex, EncryptedHeaderBlock, PlaintextHeaderBlock, cancellationToken);
+
+            return results.Select(r => new RecoveryState(this, r.Key, r.Value));
+        }
+
+        public void Write(BinaryWriter writer)
+        {
+            if (writer == null)
+            {
+                throw new ArgumentNullException(nameof(writer));
+            }
+
+            EncryptedHeaderBlock.Write(writer);
+            PlaintextHeaderBlock.Write(writer);
+
+            writer.Write(B0Blocks.Count());
+
+            foreach (var block in B0Blocks)
+            {
+                block.Write(writer);
+            }
+
+            writer.Write(SequentialBlocks.Count());
+
+            foreach (var block in SequentialBlocks)
+            {
+                block.Write(writer);
+            }
+        }
+
+        public static RecoveryState Read(BinaryReader reader)
+        {
+            if (reader == null)
+            {
+                throw new ArgumentNullException(nameof(reader));
+            }
+
+            var newPartialKey = PartialKey.Read(reader);
+            var newEncryptedHeaderBlock = Block.Read(reader);
+            var newPlaintextHeaderBlock = Block.Read(reader);
+            int size = reader.ReadInt32();
+
+            var newB0Blocks = new List<Block>(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                newB0Blocks.Add(Block.Read(reader));
+            }
+
+            var newSequentialBlocks = new List<Block>(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                newSequentialBlocks.Add(Block.Read(reader));
+            }
+
+            return new RecoveryState(newPartialKey, newB0Blocks, newSequentialBlocks, newEncryptedHeaderBlock, newPlaintextHeaderBlock);
+        }
+
+        public int CompareTo(RecoveryState other)
+        {
+            int cmp = other.B0Blocks.Count() - B0Blocks.Count();
+
+            if (cmp == 0)
+            {
+                cmp = other.SequentialBlocks.Count() - SequentialBlocks.Count();
+
+                if (cmp == 0)
+                {
+                    cmp = PartialKey.CompareTo(other.PartialKey);
+                }
+            }
+
+            return cmp;
+        }
+
+        public bool Equals(RecoveryState other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+            else
+            {
+                return PartialKey == other.PartialKey
+                    && B0Blocks.Count() == other.B0Blocks.Count()
+                    && SequentialBlocks.Count() == other.SequentialBlocks.Count();
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is RecoveryState r)
+            {
+                return Equals(r);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 2040108761;
+            hashCode = hashCode * -1521134295 + PartialKey.GetHashCode();
+            hashCode = hashCode * -1521134295 + B0Blocks.Count().GetHashCode();
+            hashCode = hashCode * -1521134295 + SequentialBlocks.Count().GetHashCode();
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return $"RecoveryState({B0Blocks.Count()},{SequentialBlocks.Count()},{PartialKey.ToString()})";
+        }
+
+        public static bool operator ==(RecoveryState left, RecoveryState right)
+        {
+            return EqualityComparer<RecoveryState>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(RecoveryState left, RecoveryState right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ TpsParser/Tps/KeyRecovery/RecoveryStateExtensions.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TpsParser.Tps.KeyRecovery
+{
+    public static class RecoveryStateExtensions
+    {
+        /// <summary>
+        /// <para>
+        /// Reduces the number of candidate solutions by finding any blocks that decrypt to a sequence (like 0x40414243).
+        /// </para>
+        /// <para>
+        /// Blocks that are found to hold a sequence are added to the recovery state for further evaluation.
+        /// </para>
+        /// </summary>
+        /// <param name="candidates"></param>
+        /// <param name="index"></param>
+        /// <param name="blocks"></param>
+        /// <returns></returns>
+        public static IEnumerable<RecoveryState> ReduceFirstSequential(this IEnumerable<RecoveryState> candidates, int index, IEnumerable<Block> blocks) =>
+            candidates
+                .Select(s => InnerReduceSequential(s, index, blocks))
+                .Where(s => s != null);
+
+        /// <summary>
+        /// Reduces the number of candidate solutions by finding any blocks that decrypt to a sequence (like 0x40414243).
+        /// </summary>
+        /// <param name="candidates"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public static IEnumerable<RecoveryState> ReduceNextSequential(this IEnumerable<RecoveryState> candidates, int index) =>
+            candidates
+                .Select(s => InnerReduceSequential(s, index, s.SequentialBlocks))
+                .Where(s => s != null);
+
+        private static RecoveryState InnerReduceSequential(RecoveryState state, int index, IEnumerable<Block> blocks)
+        {
+            var sequences = blocks
+                .Select(block => state.PartialKey.PartialDecrypt(index, block))
+                .Where(decryptedBlock => Block.IsSequencePart(decryptedBlock.Values[index]));
+
+            return sequences.Any() ? RecoveryState.CreateFromSequentialBlocks(state, sequences) : null;
+        }
+
+        /// <summary>
+        /// <para>
+        /// Reduces the number of solutions by attempting to decrypt blocks with duplicates of 0xB0B0B0B0 blocks.
+        /// </para>
+        /// <para>
+        /// If at least one of those blocks is found, the solution is kept. The blocks that decrypt to 0xB0B0 are saved in the keys
+        /// recovery state for further decryption.
+        /// </para>
+        /// </summary>
+        /// <param name="candidates"></param>
+        /// <param name="index"></param>
+        /// <param name="blocks"></param>
+        /// <returns></returns>
+        public static IEnumerable<RecoveryState> ReduceFirstB0(this IEnumerable<RecoveryState> candidates, int index, IEnumerable<Block> blocks) =>
+            candidates
+                .Select(s => InnerReduceB0(s, index, blocks))
+                .Where(s => s != null);
+
+        /// <summary>
+        /// <para>
+        /// Reduces the number of solutions by re-evaluating the found 0xB0B0 blocks at this index.
+        /// </para>
+        /// <para>
+        /// If at least one block still decrypts to 0xB0B0, the candidate is kept.
+        /// </para>
+        /// </summary>
+        /// <param name="candidates"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public static IEnumerable<RecoveryState> ReduceNextB0(this IEnumerable<RecoveryState> candidates, int index) =>
+            candidates
+                .Select(s => InnerReduceB0(s, index, s.B0Blocks))
+                .Where(s => s != null);
+
+        private static RecoveryState InnerReduceB0(RecoveryState state, int index, IEnumerable<Block> blocks)
+        {
+            var b0s = blocks
+                .Select(block => state.PartialKey.PartialDecrypt(index, block))
+                .Where(decryptedBlock => Block.IsB0Part((uint)decryptedBlock.Values[index]));
+
+            return b0s.Any() ? RecoveryState.CreateFromB0Blocks(state, b0s) : null;
+        }
+
+        public static IEnumerable<RecoveryState> ReduceFirst(this IEnumerable<RecoveryState> candidates, int index, IEnumerable<Block> blocks) =>
+            candidates
+                .ReduceFirstB0(index, blocks)
+                .ReduceFirstSequential(index, blocks);
+
+        public static IEnumerable<RecoveryState> ReduceNext(this IEnumerable<RecoveryState> candidates, int index) =>
+            candidates
+                .ReduceNextB0(index)
+                .ReduceNextSequential(index);
+    }
+}

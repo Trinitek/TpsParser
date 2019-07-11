2a7dd517db8400dbbd031c25e94dbeb54cef8c5d
Blake Burgess <trinitek@live.com>
2019-07-11 18:45:00 -0400

    Add GetValueCaseInsensitive method to Row

--- TpsParser/Row.cs
+++ TpsParser/Row.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using TpsParser.Tps.Type;
 
 namespace TpsParser
@@ -32,6 +34,37 @@ namespace TpsParser
         /// </summary>
         /// <param name="column"></param>
         /// <returns></returns>
-        public TpsObject this[string column] => Values[column];
+        public TpsObject GetValue(string column) =>  Values[column];
+
+        /// <summary>
+        /// Gets the field name, memo, or blob associated with the given column name.
+        /// </summary>
+        /// <param name="column">The case insensitive name of the column.</param>
+        /// <returns></returns>
+        public TpsObject GetValueCaseInsensitive(string column)
+        {
+            var matchingKey = Values.Keys.FirstOrDefault(k => k.Equals(column, StringComparison.OrdinalIgnoreCase));
+
+            if (matchingKey is null)
+            {
+                var sb = new StringBuilder();
+
+                foreach (var key in Values.Keys)
+                {
+                    sb.Append("key, ");
+                }
+
+                var keyList = sb.ToString();
+
+                if (Values.Keys.Any())
+                {
+                    keyList = keyList.Substring(0, keyList.Length - 2); // Trim trailing space and comma
+                }
+
+                throw new ArgumentException($"Could not find column by case insensitive name '{column}'. Available columns are [{keyList}].");
+            }
+
+            return Values[matchingKey];
+        }
     }
 }

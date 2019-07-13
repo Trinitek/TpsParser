b7906324444090879d645d09e95bc9ed89dbce0e
Blake Burgess <trinitek@live.com>
2019-07-13 00:03:41 -0400

    Add doc comments to TpsTime

--- TpsParser/Tps/Type/TpsObject.cs
+++ TpsParser/Tps/Type/TpsObject.cs
@@ -2,15 +2,30 @@
 
 namespace TpsParser.Tps.Type
 {
+    /// <summary>
+    /// Represents a typed object within the TopSpeed file.
+    /// </summary>
     public abstract class TpsObject
     {
+        /// <summary>
+        /// Gets the .NET equivalent value of the TopSpeed object.
+        /// </summary>
         public object Value { get; protected set; }
 
+        /// <summary>
+        /// Gets the type code of the object.
+        /// </summary>
         public abstract TpsTypeCode TypeCode { get; }
 
+        /// <summary>
+        /// Gets the string representation of the value that this object encapsulates.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString() => Value?.ToString();
 
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         public override bool Equals(object obj)
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
         {
             if (obj is TpsObject o)
             {
@@ -22,7 +37,9 @@ namespace TpsParser.Tps.Type
             }
         }
 
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         public override int GetHashCode()
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
         {
             var hashCode = -1431579180;
             hashCode = hashCode * -1521134295 + EqualityComparer<object>.Default.GetHashCode(Value);
@@ -30,19 +47,28 @@ namespace TpsParser.Tps.Type
             return hashCode;
         }
 
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         public static bool operator ==(TpsObject left, TpsObject right)
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
         {
             return EqualityComparer<TpsObject>.Default.Equals(left, right);
         }
 
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         public static bool operator !=(TpsObject left, TpsObject right)
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
         {
             return !(left == right);
         }
     }
 
+    /// <summary>
+    /// Represents a typed object within the TopSpeed file.
+    /// </summary>
+    /// <typeparam name="T">The .NET equivalent type of the value this object encapsulates.</typeparam>
     public abstract class TpsObject<T> : TpsObject
     {
+        /// <inheritdoc/>
         public new T Value
         {
             get => _typedValue;
--- TpsParser/Tps/Type/TpsTime.cs
+++ TpsParser/Tps/Type/TpsTime.cs
@@ -4,13 +4,57 @@ using TpsParser.Binary;
 namespace TpsParser.Tps.Type
 {
     /// <summary>
-    /// Represents a moment in time.
+    /// Represents a moment in time. Some time keeping fields you expect to be of type <see cref="TpsTime"/> may actually be of type <see cref="TpsLong"/>.
+    /// See the remarks section for details.
     /// </summary>
+    /// <remarks>
+    /// <para>
+    /// A TIME is composed of 4 bytes. The structure is composed as such:
+    /// <list type="table">
+    /// <listheader>
+    /// <term>Type</term>
+    /// <term>Description</term>
+    /// <term>Range</term>
+    /// </listheader>
+    /// <item>
+    /// <term><see cref="byte"/></term>
+    /// <term>Hours</term>
+    /// <term>0 to 23</term>
+    /// </item>
+    /// <item>
+    /// <term><see cref="byte"/></term>
+    /// <term>Minutes</term>
+    /// <term>0 to 59</term>
+    /// </item>
+    /// <item>
+    /// <term><see cref="byte"/></term>
+    /// <term>Seconds</term>
+    /// <term>0 to 59</term>
+    /// </item>
+    /// <item>
+    /// <term><see cref="byte"/></term>
+    /// <term>Centiseconds (1/100 seconds)</term>
+    /// <term>0 to 99</term>
+    /// </item>
+    /// </list>
+    /// A centisecond is 1/100th of a second.
+    /// </para>
+    /// <para>
+    /// In the Clarion programming language, when an expression is performed on a TIME data type, it is implicitly converted to a
+    /// LONG as a Clarion Standard Time value. This value is the number of centiseconds (1/100 seconds) since midnight. Clarion
+    /// documentation recommends that the 4-byte TIME type be used when communicating with external applications. However, because
+    /// TopSpeed files are typically used exclusively by Clarion applications, the field may sometimes be defined as a LONG in
+    /// order to avoid repetitive casting.
+    /// </para>
+    /// </remarks>
     public sealed class TpsTime : TpsObject<TimeSpan>
     {
+        /// <inheritdoc/>
         public override TpsTypeCode TypeCode => TpsTypeCode.Time;
 
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         public TpsTime(RandomAccess rx)
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
         {
             if (rx == null)
             {

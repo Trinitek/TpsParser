1b75bd5718d378d3d16e9309649a0d0a3ebc7186
Blake Burgess <trinitek@live.com>
2019-07-16 19:44:06 -0400

    Add XML documentation to TpsDate

--- src/TpsParser/Tps/Type/TpsDate.cs
+++ src/TpsParser/Tps/Type/TpsDate.cs
@@ -4,14 +4,50 @@ using TpsParser.Binary;
 namespace TpsParser.Tps.Type
 {
     /// <summary>
-    /// Represents a date.
+    /// Represents a date. Some time keeping fields you expect to be of type <see cref="TpsDate"/>  may actually be of type <see cref="TpsLong"/>.
+    /// See the remarks section for details.
     /// </summary>
+    /// <remarks>
+    /// <para>
+    /// A DATE is composed of 4 bytes.
+    /// <list type="table">
+    /// <listheader>
+    /// <term>Field</term>
+    /// <term>Range</term>
+    /// </listheader>
+    /// <item>
+    /// <term>Day (byte)</term>
+    /// <description>1 to 31</description>
+    /// </item>
+    /// <item>
+    /// <term>Month (byte)</term>
+    /// <description>1 to 12</description>
+    /// </item>
+    /// <item>
+    /// <term>Year (word)</term>
+    /// <description>1 to 9999</description>
+    /// </item>
+    /// </list>
+    /// </para>
+    /// <para>
+    /// In the Clarion programming language, when a DATE value is used in an expression, it is implicitly converted to a
+    /// LONG (<see cref="TpsLong"/>). This is called a Clarion Standard Date value. This value is the number of days since December 28, 1880.
+    /// The valid Clarion Standard Date range is January 1, 1801 through December 31, 9999.
+    /// </para>
+    /// <para>
+    /// Clarion documentation recommends that the 4-byte DATE type be used when communicating with external applications. However, because
+    /// TopSpeed files are typically only used by Clarion applications exclusively, the field may sometimes be defined as a LONG in
+    /// order to avoid repetitive casting.
+    /// </para>
+    /// </remarks>
     public sealed class TpsDate : TpsObject<DateTime?>
     {
         /// <inheritdoc/>
         public override TpsTypeCode TypeCode => TpsTypeCode.Date;
 
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         public TpsDate(RandomAccess rx)
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
         {
             if (rx == null)
             {

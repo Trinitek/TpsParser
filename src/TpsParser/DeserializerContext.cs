59b24a1dabe9e9237f685fb386572b84784243e0
Blake Burgess <trinitek@live.com>
2019-09-18 23:28:47 -0400

    Revert "Remove MemberInfo cache in DeserializerContext"
    This reverts commit 881623a2ea92361e92448b3721769351471b048a.

--- src/TpsParser/DeserializerContext.cs
+++ src/TpsParser/DeserializerContext.cs
@@ -7,10 +7,26 @@ namespace TpsParser
 {
     internal sealed class DeserializerContext
     {
+        private Dictionary<Type, ModelMember[]> TypeCache { get; }
+
         public DeserializerContext()
-        { }
+        {
+            TypeCache = new Dictionary<Type, ModelMember[]>();
+        }
 
         public IEnumerable<ModelMember> GetModelMembers<T>(T targetObject) where T : class
+        {
+            if (TypeCache.TryGetValue(typeof(T), out var members))
+            {
+                return members;
+            }
+            else
+            {
+                return Add(targetObject);
+            }
+        }
+
+        private IEnumerable<ModelMember> Add<T>(T targetObject) where T : class
         {
             if (targetObject is null)
             {
@@ -21,7 +37,10 @@ namespace TpsParser
 
             var modelMembers = members
                 .Select(m => ModelMember.BuildModelMember(m))
-                .Where(m => m != null);
+                .Where(m => m != null)
+                .ToArray();
+
+            TypeCache.Add(typeof(T), modelMembers);
 
             return modelMembers;
         }

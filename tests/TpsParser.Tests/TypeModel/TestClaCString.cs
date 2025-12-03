e7d6090865ca7d7b0c076752b47331483e4cd808
Blake Burgess <trinitek@LIVE.COM>
2025-12-03 00:39:15 -0500

    Test coverage for string type equality

--- tests/TpsParser.Tests/TypeModel/TestClaCString.cs
+++ tests/TpsParser.Tests/TypeModel/TestClaCString.cs
@@ -30,4 +30,99 @@ internal sealed class TestClaCString
     {
         Assert.Throws<ArgumentNullException>(() => new ClaCString((string)null!));
     }
+
+    [Test]
+    public void Equality()
+    {
+        var s_str1 = new ClaCString("Foo ");
+        var s_str2 = new ClaCString("Foo ");
+        var s_mem1 = new ClaCString("Foo "u8.ToArray());
+        var s_mem2 = new ClaCString("Foo "u8.ToArray());
+
+        using (Assert.EnterMultipleScope())
+        {
+#pragma warning disable CS1718 // Comparison made to same variable
+#pragma warning disable NUnit2010 // Use EqualConstraint for better assertion messages in case of failure
+
+            Assert.That(s_str1 == s_str1);
+            Assert.That(s_str1 == s_str2);
+
+            Assert.That(s_str2 == s_str1);
+            Assert.That(s_str2 == s_str2);
+
+            Assert.That(s_mem1 == s_mem1);
+            Assert.That(s_mem1 == s_mem2);
+
+            Assert.That(s_mem2 == s_mem1);
+            Assert.That(s_mem2 == s_mem2);
+
+#pragma warning restore NUnit2010 // Use EqualConstraint for better assertion messages in case of failure
+#pragma warning restore CS1718 // Comparison made to same variable
+        }
+    }
+
+    [Test]
+    public void Inequality_StringsNotEquatableToMemory()
+    {
+        var s_str1 = new ClaCString("Foo ");
+        var s_str2 = new ClaCString("Foo ");
+        var s_mem1 = new ClaCString("Foo "u8.ToArray());
+        var s_mem2 = new ClaCString("Foo "u8.ToArray());
+
+        using (Assert.EnterMultipleScope())
+        {
+#pragma warning disable CS1718 // Comparison made to same variable
+#pragma warning disable NUnit2010 // Use EqualConstraint for better assertion messages in case of failure
+
+            Assert.That(s_str1 != s_mem1);
+            Assert.That(s_str1 != s_mem2);
+
+            Assert.That(s_str2 != s_mem1);
+            Assert.That(s_str2 != s_mem2);
+
+            Assert.That(s_mem1 != s_str1);
+            Assert.That(s_mem1 != s_str2);
+
+            Assert.That(s_mem2 != s_str1);
+            Assert.That(s_mem2 != s_str2);
+
+#pragma warning restore NUnit2010 // Use EqualConstraint for better assertion messages in case of failure
+#pragma warning restore CS1718 // Comparison made to same variable
+        }
+    }
+
+    [Test]
+    public void Inequality()
+    {
+        var s_str1 = new ClaCString("Foo1 ");
+        var s_str2 = new ClaCString("Foo2 ");
+        var s_mem1 = new ClaCString("Foo3 "u8.ToArray());
+        var s_mem2 = new ClaCString("Foo4 "u8.ToArray());
+
+        using (Assert.EnterMultipleScope())
+        {
+#pragma warning disable CS1718 // Comparison made to same variable
+#pragma warning disable NUnit2010 // Use EqualConstraint for better assertion messages in case of failure
+
+            Assert.That(s_str1 != s_str2);
+            Assert.That(s_str1 != s_mem1);
+            Assert.That(s_str1 != s_mem2);
+
+            Assert.That(s_str2 != s_str1);
+            Assert.That(s_str2 != s_mem1);
+            Assert.That(s_str2 != s_mem2);
+
+            Assert.That(s_mem1 != s_str1);
+            Assert.That(s_mem1 != s_str2);
+            Assert.That(s_mem1 != s_mem2);
+
+            Assert.That(s_mem2 != s_str1);
+            Assert.That(s_mem2 != s_str2);
+            Assert.That(s_mem2 != s_mem1);
+            
+
+#pragma warning restore NUnit2010 // Use EqualConstraint for better assertion messages in case of failure
+#pragma warning restore CS1718 // Comparison made to same variable
+        }
+    }
 }
--- tests/TpsParser.Tests/TypeModel/TestClaFString.cs
+++ tests/TpsParser.Tests/TypeModel/TestClaFString.cs
@@ -30,4 +30,99 @@ internal sealed class TestClaFString
     {
         Assert.Throws<ArgumentNullException>(() => new ClaFString((string)null!));
     }
+
+    [Test]
+    public void Equality()
+    {
+        var s_str1 = new ClaFString("Foo ");
+        var s_str2 = new ClaFString("Foo ");
+        var s_mem1 = new ClaFString("Foo "u8.ToArray());
+        var s_mem2 = new ClaFString("Foo "u8.ToArray());
+
+        using (Assert.EnterMultipleScope())
+        {
+#pragma warning disable CS1718 // Comparison made to same variable
+#pragma warning disable NUnit2010 // Use EqualConstraint for better assertion messages in case of failure
+
+            Assert.That(s_str1 == s_str1);
+            Assert.That(s_str1 == s_str2);
+
+            Assert.That(s_str2 == s_str1);
+            Assert.That(s_str2 == s_str2);
+
+            Assert.That(s_mem1 == s_mem1);
+            Assert.That(s_mem1 == s_mem2);
+
+            Assert.That(s_mem2 == s_mem1);
+            Assert.That(s_mem2 == s_mem2);
+
+#pragma warning restore NUnit2010 // Use EqualConstraint for better assertion messages in case of failure
+#pragma warning restore CS1718 // Comparison made to same variable
+        }
+    }
+
+    [Test]
+    public void Inequality_StringsNotEquatableToMemory()
+    {
+        var s_str1 = new ClaFString("Foo ");
+        var s_str2 = new ClaFString("Foo ");
+        var s_mem1 = new ClaFString("Foo "u8.ToArray());
+        var s_mem2 = new ClaFString("Foo "u8.ToArray());
+
+        using (Assert.EnterMultipleScope())
+        {
+#pragma warning disable CS1718 // Comparison made to same variable
+#pragma warning disable NUnit2010 // Use EqualConstraint for better assertion messages in case of failure
+
+            Assert.That(s_str1 != s_mem1);
+            Assert.That(s_str1 != s_mem2);
+
+            Assert.That(s_str2 != s_mem1);
+            Assert.That(s_str2 != s_mem2);
+
+            Assert.That(s_mem1 != s_str1);
+            Assert.That(s_mem1 != s_str2);
+
+            Assert.That(s_mem2 != s_str1);
+            Assert.That(s_mem2 != s_str2);
+
+#pragma warning restore NUnit2010 // Use EqualConstraint for better assertion messages in case of failure
+#pragma warning restore CS1718 // Comparison made to same variable
+        }
+    }
+
+    [Test]
+    public void Inequality()
+    {
+        var s_str1 = new ClaFString("Foo1 ");
+        var s_str2 = new ClaFString("Foo2 ");
+        var s_mem1 = new ClaFString("Foo3 "u8.ToArray());
+        var s_mem2 = new ClaFString("Foo4 "u8.ToArray());
+
+        using (Assert.EnterMultipleScope())
+        {
+#pragma warning disable CS1718 // Comparison made to same variable
+#pragma warning disable NUnit2010 // Use EqualConstraint for better assertion messages in case of failure
+
+            Assert.That(s_str1 != s_str2);
+            Assert.That(s_str1 != s_mem1);
+            Assert.That(s_str1 != s_mem2);
+
+            Assert.That(s_str2 != s_str1);
+            Assert.That(s_str2 != s_mem1);
+            Assert.That(s_str2 != s_mem2);
+
+            Assert.That(s_mem1 != s_str1);
+            Assert.That(s_mem1 != s_str2);
+            Assert.That(s_mem1 != s_mem2);
+
+            Assert.That(s_mem2 != s_str1);
+            Assert.That(s_mem2 != s_str2);
+            Assert.That(s_mem2 != s_mem1);
+
+
+#pragma warning restore NUnit2010 // Use EqualConstraint for better assertion messages in case of failure
+#pragma warning restore CS1718 // Comparison made to same variable
+        }
+    }
 }
